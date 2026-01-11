0f88f0e9115c3247f21104468c66d89eb2b61e19
amsyeung <yeung.ming.sang@gmail.com>
2026-01-11 11:34:18 +0800

    feat: add 7 Reverse Integer solution

---
+++ neetcode 150/bit manipulation/7. Reverse Integer.py
@@ -0,0 +1,56 @@
+"""
+7. Reverse Integer
+Medium
+Topics
+premium lock icon
+Companies
+Given a signed 32-bit integer x, return x with its digits reversed. If reversing x causes the value to go outside the signed 32-bit integer range [-231, 231 - 1], then return 0.
+
+Assume the environment does not allow you to store 64-bit integers (signed or unsigned).
+
+ 
+
+Example 1:
+
+Input: x = 123
+Output: 321
+Example 2:
+
+Input: x = -123
+Output: -321
+Example 3:
+
+Input: x = 120
+Output: 21
+ 
+
+Constraints:
+
+-231 <= x <= 231 - 1
+"""
+
+class Solution:
+    def reverse(self, x: int) -> int:
+        INT_MIN = -2 ** 31
+        INT_MAX = 2 ** 31 - 1
+        sign = -1 if x < 0 else 1
+        s = list(str(abs(x)))
+        l, r = 0, len(s) - 1
+        while l < r:
+            s[l], s[r] = s[r], s[l]
+            l, r = l + 1, r - 1
+        res = sign * int("".join(s))
+        if res < INT_MIN or res > INT_MAX:
+            return 0
+        else:
+            return res
+
+solution = Solution()
+print(solution.reverse(123)) # 321
+print(solution.reverse(-123)) # -321
+print(solution.reverse(120)) # 21
+
+"""
+Time Complexity: O(n), iterating n characters of list(str(abs(x)))
+Space Complexity: O(n), n characters store into list
+"""

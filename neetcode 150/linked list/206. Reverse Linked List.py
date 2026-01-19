30091be51bac8e77b49eb2438e5b3271a4b9d581
amsyeung <yeung.ming.sang@gmail.com>
2026-01-19 23:25:40 +0800

    feat: add 206. Reverse Linked List

---
+++ neetcode 150/linked list/206. Reverse Linked List.py
@@ -0,0 +1,53 @@
+"""
+206. Reverse Linked List
+Easy
+Topics
+premium lock icon
+Companies
+Given the head of a singly linked list, reverse the list, and return the reversed list.
+
+ 
+
+Example 1:
+
+
+Input: head = [1,2,3,4,5]
+Output: [5,4,3,2,1]
+Example 2:
+
+
+Input: head = [1,2]
+Output: [2,1]
+Example 3:
+
+Input: head = []
+Output: []
+"""
+
+from typing import ListNode, Optional
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prev = None
+        cur = head
+        while cur:
+            nxt = cur.next
+            cur.next = prev
+            prev = cur
+            cur = nxt
+        return prev
+    
+solution = Solution()
+print(solution.reverseList([1, 2])) # [2, 1]
+print(solution.reverseList([])) # []
+
+"""
+Time Complexity: O(n), shift one by one, n times
+Space Complexity: O(1), only constant variables
+I hate linked list
+"""

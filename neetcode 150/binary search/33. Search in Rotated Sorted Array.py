c6edf79118e5d5dfebfb912625159b30abdc54df
andyyms <yeung.ming.sang@gmail.com>
2026-03-11 11:32:55 +0800

    feat: add 33. Search in Rotated Sorted Array solution

---
+++ neetcode 150/binary search/33. Search in Rotated Sorted Array.py
@@ -0,0 +1,63 @@
+"""
+33. Search in Rotated Sorted Array
+Medium
+Topics
+premium lock icon
+Companies
+There is an integer array nums sorted in ascending order (with distinct values).
+
+Prior to being passed to your function, nums is possibly left rotated at an unknown index k (1 <= k < nums.length) such that the resulting array is [nums[k], nums[k+1], ..., nums[n-1], nums[0], nums[1], ..., nums[k-1]] (0-indexed). For example, [0,1,2,4,5,6,7] might be left rotated by 3 indices and become [4,5,6,7,0,1,2].
+
+Given the array nums after the possible rotation and an integer target, return the index of target if it is in nums, or -1 if it is not in nums.
+
+You must write an algorithm with O(log n) runtime complexity.
+
+ 
+
+Example 1:
+
+Input: nums = [4,5,6,7,0,1,2], target = 0
+Output: 4
+Example 2:
+
+Input: nums = [4,5,6,7,0,1,2], target = 3
+Output: -1
+Example 3:
+
+Input: nums = [1], target = 0
+Output: -1
+"""
+
+from typing import List
+
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        l, r = 0, len(nums) - 1
+        while l <= r:
+            mid = l + (r - l) // 2
+            if nums[mid] == target:
+                return mid
+            if nums[l] <= nums[mid]:
+                if nums[l] <= target and target < nums[mid]:
+                    r = mid - 1
+                else:
+                    l = mid + 1
+            else:
+                if nums[mid] < target and target <= nums[r]:
+                    l = mid + 1
+                else:
+                    r = mid - 1
+        return -1
+    
+solution = Solution()
+print(solution.search([4,5,6,7,0,1,2], 0)) # 4
+print(solution.search([4,5,6,7,0,1,2], 3)) # -1
+print(solution.search([1], 0)) # -1
+print(solution.search([1, 3], 0)) # -1
+print(solution.search([1, 3], 1)) # 0
+print(solution.search([1, 3, 5], 1)) # 0
+
+"""
+Time Complexity: O(logN)
+Space Complexity: O(1), only l, r, mid
+"""

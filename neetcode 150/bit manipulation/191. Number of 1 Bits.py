5540c430a474da46b60e10718d3ecc39b6b5978b
andyyms <yeung.ming.sang@gmail.com>
2026-03-19 22:22:15 +0800

    feat: add 191. Number of 1 Bits solution

---
+++ neetcode 150/bit manipulation/191. Number of 1 Bits.py
@@ -0,0 +1,59 @@
+"""
+191. Number of 1 Bits
+Solved
+Easy
+Topics
+premium lock icon
+Companies
+Given a positive integer n, write a function that returns the number of set bits in its binary representation (also known as the Hamming weight).
+
+ 
+
+Example 1:
+
+Input: n = 11
+
+Output: 3
+
+Explanation:
+
+The input binary string 1011 has a total of three set bits.
+
+Example 2:
+
+Input: n = 128
+
+Output: 1
+
+Explanation:
+
+The input binary string 10000000 has a total of one set bit.
+
+Example 3:
+
+Input: n = 2147483645
+
+Output: 30
+
+Explanation:
+
+The input binary string 1111111111111111111111111111101 has a total of thirty set bits.
+"""
+
+class Solution:
+    def hammingWeight(self, n: int) -> int:
+        res = 0
+        while n:
+            res += n & 1
+            n >>= 1
+        return res
+    
+solution = Solution()
+print(solution.hammingWeight(11)) # 3
+print(solution.hammingWeight(128)) # 1
+print(solution.hammingWeight(2147483645)) # 30
+
+"""
+Time Complexity: O(n), where n is number of bits
+Space Complexity: O(1)
+"""

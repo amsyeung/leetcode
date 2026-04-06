9c1f3902132c18dbcb1ab3e695bd08ddd00ac190
andyyms <yeung.ming.sang@gmail.com>
2026-04-06 22:23:00 +0800

    feat: add 105. Construct Binary Tree from Preorder and Inorder Traversal solution

---
+++ neetcode 150/tree/105. Construct Binary Tree from Preorder and Inorder Traversal.py
@@ -0,0 +1,48 @@
+"""
+105. Construct Binary Tree from Preorder and Inorder Traversal
+Medium
+Topics
+premium lock icon
+Companies
+Given two integer arrays preorder and inorder where preorder is the preorder traversal of a binary tree and inorder is the inorder traversal of the same tree, construct and return the binary tree.
+
+ 
+
+Example 1:
+
+
+Input: preorder = [3,9,20,15,7], inorder = [9,3,15,20,7]
+Output: [3,9,20,null,null,15,7]
+Example 2:
+
+Input: preorder = [-1], inorder = [-1]
+Output: [-1]
+ 
+
+Constraints:
+
+1 <= preorder.length <= 3000
+inorder.length == preorder.length
+-3000 <= preorder[i], inorder[i] <= 3000
+preorder and inorder consist of unique values.
+Each value of inorder also appears in preorder.
+preorder is guaranteed to be the preorder traversal of the tree.
+inorder is guaranteed to be the inorder traversal of the tree.
+"""
+
+from typing import Optional, List
+from treeutil import TreeUtil, TreeNode
+
+class Solution: # Time: O(N^2), Space: O(N^2)
+    def buildTree(self, preorder: List[int], inorder: List[int]) -> Optional[TreeNode]:
+        if not preorder or not inorder:
+            return None
+        root_val = preorder[0]
+        root = TreeNode(root_val)
+        mid = inorder.index(root_val)
+        root.left = self.buildTree(preorder[1:mid+1], inorder[:mid])
+        root.right = self.buildTree(preorder[mid+1:], inorder[mid+1:]) 
+        return root
+    
+solution = Solution()
+TreeUtil.print_tree(solution.buildTree([3,9,20,15,7], [9,3,15,20,7]))

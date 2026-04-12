24b9047c6140583b77765718076f703a618c0740
andyyms <yeung.ming.sang@gmail.com>
2026-04-12 11:44:42 +0800

    refactor: q139 change list to set

--- neetcode 150/1-D Dynamic Programming/139. Word Break.py
+++ neetcode 150/1-D Dynamic Programming/139. Word Break.py
@@ -34,9 +34,10 @@ class Solution:
         n = len(s)
         dp = [False] * (n + 1)
         dp[0] = True
+        word_set = set(wordDict)
         for i in range(1, n + 1):
             for j in range(i):
-                if dp[j] and s[j:i] in wordDict:
+                if dp[j] and s[j:i] in word_set:
                     dp[i] = True
                     break
         return dp[n]

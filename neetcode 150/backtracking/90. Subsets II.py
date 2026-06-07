f060479bb2fe765104f4626e128037574142a1f5
amsyeung <yeung.ming.sang@gmail.com>
2026-06-07 14:58:38 +0800

    feat: add 90. Subsets II solution

---
+++ neetcode 150/backtracking/90. Subsets II.py
@@ -0,0 +1,41 @@
+"""
+90. Subsets II
+Medium
+Topics
+premium lock icon
+Companies
+Given an integer array nums that may contain duplicates, return all possible subsets (the power set).
+
+The solution set must not contain duplicate subsets. Return the solution in any order.
+
+ 
+
+Example 1:
+
+Input: nums = [1,2,2]
+Output: [[],[1],[1,2],[1,2,2],[2],[2,2]]
+Example 2:
+
+Input: nums = [0]
+Output: [[],[0]]
+"""
+
+from typing import List
+
+class Solution:
+    def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
+        nums.sort()
+        res = []
+        def dfs(start: int, cur: List):
+            res.append(cur.copy())
+            for i in range(start, len(nums)):
+                if i > start and nums[i] == nums[i-1]:
+                    continue
+                cur.append(nums[i])
+                dfs(i + 1, cur)
+                cur.pop()
+        dfs(0, [])
+        return res
+    
+s = Solution()
+print(s.subsetsWithDup([1, 2, 2])) # [[],[1],[1,2],[1,2,2],[2],[2,2]]
